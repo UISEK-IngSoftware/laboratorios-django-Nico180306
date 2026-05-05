634f36c4aad0f850e6ccc1bf0faf8ee3577dddf5
laboratorio.fs <christian.castro@uisek.edu.ec>
2026-05-05 10:47:30 -0500

    Laboratorio 4 Finalizado

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Pokemon
 
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'height', 'weight')
 # Register your models here.
