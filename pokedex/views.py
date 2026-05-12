875ea21a2a03a203c0e60188b9749b8c2386bbac
laboratorio.fs <christian.castro@uisek.edu.ec>
2026-05-12 10:19:21 -0500

    Lab 5: Creación y personalización de Formularios y CRUD de registros

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Pokemon, Trainer
+from django.shortcuts import redirect, render
+from pokedex.forms import PokemonForm
 
 def index(request):
     pokemons = Pokemon.objects.all() #select * from pokedex_pokemon
@@ -23,3 +25,29 @@ def trainer(request, id: int):
         'trainer': trainer
     }
     return HttpResponse(template.render(context, request))
+
+def add_pokemon(request):
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm()
+    return render(request, 'pokemon_form.html', {'form': form})
+
+def edit_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm(instance=pokemon)
+    return render(request, 'pokemon_form.html', {'form': form})
+
+def delete_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id) #select * from pokedex_pokemon where id = pokemon_id
+    pokemon.delete() #delete from pokedex_pokemon where id = pokemon_id
+    return redirect('pokedex:index')
