7f53a854d204f6fe7cb47fafe400c74cfd396bac
Nicolas <nicolas.santillan@uisek.edu.ec>
2026-06-01 01:02:01 -0500

    CRUD de entrenadores y ajustes corregidos

--- pokedex/forms.py
+++ pokedex/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Pokemon
+from .models import Pokemon, Trainer
 
 class PokemonForm(forms.ModelForm):
     class Meta:
@@ -20,4 +20,21 @@ class PokemonForm(forms.ModelForm):
             'type': forms.Select(attrs={'class': 'form-control'}),
             'trainer': forms.Select(attrs={'class': 'form-control'}),
             'picture': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
+
+class TrainerForm(forms.ModelForm):
+    class Meta:
+        model = Trainer
+        fields = '__all__'
+        labels = {
+            'first_name': 'Nombre',
+            'last_name': 'Apellido',
+            'birth_date': 'Fecha de nacimiento',
+            'level': 'Nivel',
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'birth_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'level': forms.NumberInput(attrs={'class': 'form-control'}),
         }
--- pokedex/views.py
+++ pokedex/views.py
@@ -3,6 +3,8 @@ from django.template import loader
 from .models import Pokemon, Trainer
 from django.shortcuts import redirect, render
 from pokedex.forms import PokemonForm
+from pokedex.forms import TrainerForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 
@@ -17,14 +19,6 @@ def pokemon(request, id: int):
     template = loader.get_template('display_pokemon.html')
     context = {
         'pokemon': pokemon
-    }
-    return HttpResponse(template.render(context, request))
-
-def trainer(request, id: int):
-    trainer = Trainer.objects.get(id=id) #select * from pokedex_trainer where id = trainer_id
-    template = loader.get_template('display_trainer.html')
-    context = {
-        'trainer': trainer
     }
     return HttpResponse(template.render(context, request))
 
@@ -55,6 +49,43 @@ def edit_pokemon(request, pokemon_id):
 def delete_pokemon(request, pokemon_id):
     pokemon = Pokemon.objects.get(id=pokemon_id) #select * from pokedex_pokemon where id = pokemon_id
     pokemon.delete() #delete from pokedex_pokemon where id = pokemon_id
+    return redirect('pokedex:index')
+
+def trainer(request, id: int):
+    trainer = Trainer.objects.get(id=id) #select * from pokedex_trainer where id = trainer_id
+    template = loader.get_template('display_trainer.html')
+    context = {
+        'trainer': trainer
+    }
+    return HttpResponse(template.render(context, request))
+
+@login_required
+def add_trainer(request):
+    if request.method == 'POST':
+        form = TrainerForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = TrainerForm()
+    return render(request, 'trainer_form.html', {'form': form})
+
+@login_required
+def edit_trainer(request, trainer_id):
+    trainer = Trainer.objects.get(id=trainer_id)
+    if request.method == 'POST':
+        form = TrainerForm(request.POST, request.FILES, instance=trainer)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = TrainerForm(instance=trainer)
+    return render(request, 'trainer_form.html', {'form': form})
+
+@login_required
+def delete_trainer(request, trainer_id):
+    trainer = Trainer.objects.get(id=trainer_id) #select * from pokedex_trainer where id = trainer_id
+    trainer.delete() #delete from pokedex_trainer where id = trainer_id
     return redirect('pokedex:index')
 
 class CustomLoginView(LoginView):
